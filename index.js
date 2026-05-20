88d99fae490844ad679a56002b5dcb40ee183006
Baseer Khan <baseerkhan@gmail.com>
2026-05-20 23:48:41 +0500

    Add date format and new route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT;
 
@@ -19,38 +19,41 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
- await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 //Index Route
-app.get("/chats",async (req, res)=>{
+app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
   console.log(chats);
-  res.render("index.ejs",{chats});
-  
+  res.render("index.ejs", { chats });
 });
 
 // New Route
-app.get("/chats/new", (req, res)=>{
+app.get("/chats/new", (req, res) => {
   res.render("new.ejs");
 });
 
 // Create Route
-app.post("/chats",(req,res)=>{
-       let {from, to, msg} = req.body;
-       let newChat = new Chat({
-        from: from,
-        to: to,
-        msg: msg,
-        created_at: new Date()
-       });
-       console.log(newChat);
-       res.send("working");
-       
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date(),
+  });
+  newChat.save()
+    .then((res) => {
+      console.log("Chat was saved");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/chats");
 });
-
 
 app.get("/", (req, res) => {
   res.send("root is working");
